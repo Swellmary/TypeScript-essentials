f91fce91825b26e40690f66525582b2bbddfb52e
Well Mary Suarez <suarezwellmary@gmail.com>
2026-01-20 12:49:01 +0800

    Revert "added README.md file"
    This reverts commit 5bc90886208181180d05c13cde9460a5092265c2.

--- typescript-essentials.ts
+++ typescript-essentials.ts
@@ -6,10 +6,3 @@ let ids: number[] = [1, 2, 3, 4, 5]; // when it's array just add [] after type
 let x: any = "well mary";
 let xArr: any[] = [1, true, "hello"];
 let person: [number, string, boolean] = [1, "John", true]; // Tuple
-let employee: [number, string][]; // Array of tuples
-employee = [
-    [1, "Alice"],
-    [2, "Bob"],
-    [3, "Charlie"]
-]
-let pid: number | string; // Union
