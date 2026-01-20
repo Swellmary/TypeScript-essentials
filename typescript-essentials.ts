61f0fc354150040b4ea6dd52c5991a45f9116817
Swellmary <suarezwellmary@gmail.com>
2026-01-20 11:18:00 +0800

    new variable, git testing here

--- typescript-essentials.ts
+++ typescript-essentials.ts
@@ -5,3 +5,4 @@ let isPublished: boolean = true;
 let ids: number[] = [1, 2, 3, 4, 5]; // when it's array just add [] after type
 let x: any = "well mary";
 let xArr: any[] = [1, true, "hello"];
+let person: [number, string, boolean] = [1, "John", true]; // Tuple
