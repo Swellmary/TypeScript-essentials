bfc249ea4419497b49686e269088ce0dfe5925b0
Well Mary Suarez <suarezwellmary@gmail.com>
2026-01-21 18:46:11 +0800

    if we difine it inaccurately it will gives us an error

--- typescript-essentials.ts
+++ typescript-essentials.ts
@@ -8,7 +8,5 @@ interface UserInterface { // Capital letter on object name first
 const User: UserInterface = { // user: UserInterface - this is how we add object blueprint to our object.
     id: 2,
     name: "Alice", 
-    age: 23
+    age: "23"; // Error: Type 'string' is not assignable to type 'number'.
 }
-
-User.age = "23";
