1f6dce069c8aef88abb80c948cbb741156c64187
Well Mary Suarez <suarezwellmary@gmail.com>
2026-01-21 17:41:32 +0800

    TypeScript on a variables

--- typescript-essentials.ts
+++ typescript-essentials.ts
@@ -5,4 +5,3 @@ let isPublished: boolean = true;
 let ids: number[] = [1, 2, 3, 4, 5]; // when it's array just add [] after type
 let x: any = "well mary";
 let xArr: any[] = [1, true, "hello"];
-let person: [number, string, boolean] = [1, "John", true]; // Tuple
