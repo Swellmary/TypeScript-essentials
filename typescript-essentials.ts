1d758d08b2ae27a123d65b4bd76ed902ea10930f
Well Mary Suarez <suarezwellmary@gmail.com>
2026-01-21 19:16:42 +0800

    how to add when you have a func arguments on enum

--- typescript-essentials.ts
+++ typescript-essentials.ts
@@ -3,7 +3,7 @@ interface UserInterface {
     id: number;
     name: string;
     age?: number; // optional property is either a number or undefined.
-    greet(): string;
+    greet(message: string): string;
 }
 
 const User: UserInterface = { // user: UserInterface - this is how we add object blueprint to our object.
