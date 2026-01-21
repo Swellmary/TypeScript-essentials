652215fdecc8eec5d628b2fabd4c72b7c7514fb1
Well Mary Suarez <suarezwellmary@gmail.com>
2026-01-21 18:47:50 +0800

    age?: number -- will make the object optional

--- typescript-essentials.ts
+++ typescript-essentials.ts
@@ -2,11 +2,10 @@
 interface UserInterface { // Capital letter on object name first
     id: number;
     name: string;
-    age: number;
+    age?: number;
 }
 
 const User: UserInterface = { // user: UserInterface - this is how we add object blueprint to our object.
     id: 2,
     name: "Alice", 
-    age: "23"; // Error: Type 'string' is not assignable to type 'number'.
 }
