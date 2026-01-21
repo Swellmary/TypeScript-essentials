26f8bac4e816508c43797c488ebffe79e5075e8f
Well Mary Suarez <suarezwellmary@gmail.com>
2026-01-21 18:43:19 +0800

    add interface object for our object blueprint of how it should look like

--- typescript-essentials.ts
+++ typescript-essentials.ts
@@ -1,15 +1,14 @@
-// let id: number = 5; 
-// let company: string = "Acme Corp";
-// let isPublished: boolean = true;
 
-// let ids: number[] = [1, 2, 3, 4, 5]; // when it's array just add [] after type
-// let x: any = "well mary";
-// let xArr: any[] = [1, true, "hello"];
-
-
-const concatenateValues = (a: string, b: string): string => {
-    return a + b;
+interface UserInterface { // Capital letter on object name first
+    id: number;
+    name: string;
+    age: number;
 }
 
-console.log(concatenateValues("Hello, ", "world!")); // Output: Hello, world!
-console.log(concatenateValues("5", "10")); // Output: 510
+const User: UserInterface = { // git 
+    id: 2,
+    name: "Alice", 
+    age: 23
+}
+
+User.age = "23";
