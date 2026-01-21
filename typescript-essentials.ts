e1ab4b8ec335303791c8cb49a1e5b7c912ff371f
Well Mary Suarez <suarezwellmary@gmail.com>
2026-01-21 17:47:33 +0800

    no set typescript value: Hello, world! -- 15

--- typescript-essentials.ts
+++ typescript-essentials.ts
@@ -1,7 +1,15 @@
-let id: number = 5; 
-let company: string = "Acme Corp";
-let isPublished: boolean = true;
+// let id: number = 5; 
+// let company: string = "Acme Corp";
+// let isPublished: boolean = true;
 
-let ids: number[] = [1, 2, 3, 4, 5]; // when it's array just add [] after type
-let x: any = "well mary";
-let xArr: any[] = [1, true, "hello"];
+// let ids: number[] = [1, 2, 3, 4, 5]; // when it's array just add [] after type
+// let x: any = "well mary";
+// let xArr: any[] = [1, true, "hello"];
+
+
+const concatenateValues = (a, b) => {
+    return a + b;
+}
+
+console.log(concatenateValues("Hello, ", "world!")); // Output: Hello, world!
+console.log(concatenateValues(5, 10)); // Output: 15
