bbf806af5ff91d73465f2a732682fd226eba464b
Well Mary Suarez <suarezwellmary@gmail.com>
2026-01-22 20:56:11 +0800

    comments of last example

--- typescript-essentials.js
+++ typescript-essentials.js
@@ -1,26 +1,13 @@
 "use strict";
-// unauthorized, user doesn't exist, wrong credentials, internal
 Object.defineProperty(exports, "__esModule", { value: true });
-var LoginError;
-(function (LoginError) {
-    LoginError["Unauthorized"] = "unauthorized";
-    LoginError["NoUser"] = "user doesn't exist";
-    LoginError["WrongCredentials"] = "wrong credentials";
-    LoginError["Internal"] = "internal";
-})(LoginError || (LoginError = {}));
-const printErrorMsg = (error) => {
-    if (error == LoginError.Unauthorized) {
-        console.log("User not authorized");
-    }
-    else if (error == LoginError.NoUser) {
-        console.log("No user exist");
-    }
-    else if (error == LoginError.WrongCredentials) {
-        console.log("wrong credentials");
-    }
-    else if (error == LoginError.Internal) {
-        console.log("internal error");
-    }
+const User = {
+    id: 2,
+    name: "Alice",
 };
-printErrorMsg(LoginError.Unauthorized);
+if (!User.age) {
+    console.log("No age provided");
+}
+else {
+    console.log(`User age is ${User.age}`);
+}
 //# sourceMappingURL=typescript-essentials.js.map
--- typescript-essentials.ts
+++ typescript-essentials.ts
@@ -2,34 +2,64 @@
 
 
 
-
-
-
 /*
 >>>> REVERT <<<<<
 
 // unauthorized, user doesn't exist, wrong credentials, internal
 
-enum LoginError {
-    Unauthorized = "unauthorized",
-    NoUser = "user doesn't exist",
-    WrongCredentials = "wrong credentials",
-    Internal = "internal"
+<<<<<<< HEAD
+// type IDFieldType = string | number;
+
+// const printID = (id: IDFieldType) => {
+//     console.log("ID: " + id);
+// }
+
+// console.log(printID);
+
+// const printIDNow = printID("this is text on id :: " + 323);
+// console.log(printIDNow);
+
+interface BusinessPartner {
+    name: string;
+    creditScore: number;
 }
 
-
-const printErrorMsg = (error: LoginError) => {
-    if (error == LoginError.Unauthorized) {
-        console.log("User not authorized")
-    } else if (error == LoginError.NoUser) {
-        console.log("No user exist")
-    } else if (error == LoginError.WrongCredentials){
-        console.log("wrong credentials")
-    } else if (error == LoginError.Internal) {
-        console.log ("internal error")
-    }
+interface UserIdentity {
+    id: number;
+    email: string;
 }
 
+<<<<<<< HEAD
 printErrorMsg(LoginError.Unauthorized);
 
 */
+/*
+=======
+type Employee = BusinessPartner & UserIdentity
+
+const signContract = (employee: Employee): void => {
+    console.log("Contract signed by: " + employee.name + "With email: " + employee.email)
+}
+
+signContract({name: "Well Mary", creditScore: 800, id:53, email: "suarezwellmary@gmail.co"})
+=======
+
+*/
+// interface UserInterface {
+//     id: number;
+//     name: string;
+//     age?: number; // optional property is either a number or undefined.
+//     greet(message: string): void;
+// }
+
+// const User: UserInterface = { // user: UserInterface - this is how we add object blueprint to our object.
+//     id: 2,
+//     name: "Alice", 
+//     greet(message) {
+//         console.log(message);
+//     }
+// }
+
+// User.greet("Hello, Alice!"); 
+>>>>>>> parent of 3b0a41d (add modern function with typescript and display differently)
+>>>>>>> parent of 29c0974 (add enum: define error keys)
